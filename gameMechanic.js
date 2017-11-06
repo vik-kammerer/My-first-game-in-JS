daed7b8c6599f76e9c14c05aab5bfdf65157c806
unknown <vik.kammerer@gmail.com>
2017-11-06 11:16:03 +0200

    Refactoting in progress

--- gameMechanic.js
+++ gameMechanic.js
@@ -15,31 +15,26 @@ tryAgain		=	document.getElementById("tryAgain");
 killedMonst		=	0;
 
 
-function displayTryAgainBtn()
-	{
+function displayTryAgainBtn() {
 		tryAgain.style.display = "block";
 	}							
 								
-function updateKilledCounter()
-	{
+function updateKilledCounter() {
 		killedCounter.innerHTML = killedMonst;
 	}
 
-function checkIfNotAWinner()
-	{
-		if (killedMonst >= 5)
-		{	
+function checkIfNotAWinner() {
+		if (killedMonst >= 5) {	
 			storyLine.append(" You are the winner!");
 			runBtn.disabled = true;
 			atackBtn.disabled = true;
 			keepGoingBtn.disabled = true;
-			displayTryAgainBtn()
+			displayTryAgainBtn();
 		}
 	}
 
 								
-function keepGoing()
-	{
+function keepGoing() {
 		atackBtn.disabled = false;
 		runBtn.disabled = false;
 		keepGoingBtn.disabled = true;
@@ -47,85 +42,70 @@ function keepGoing()
 		
 	}
 								
-function refresh()
-	{
-		storyLine.innerHTML = "You have met your foe. What will you do?"
+function refresh() {
+		storyLine.innerHTML = "You have met your foe. What will you do?";
 	}
 								
 								
 								
-function battle()
-	{
+function battle() {
 		var yourLuck;
 		yourLuck = Math.random();
 									
 									
-		if (yourLuck < 0.35)
-			{
+		if (yourLuck < 0.35) {
 				storyLine.innerHTML = "You are dead!"
 				runBtn.disabled = true;
 				atackBtn.disabled = true;
 				keepGoingBtn.disabled = true;
-				displayTryAgainBtn()
+				displayTryAgainBtn();
 				console.log("You luck is " + yourLuck);
-
-
-
 			}
 			
 
 		
-		else
-			{
+		else {
 				storyLine.innerHTML = "The monster has been killed!"
 				killedMonst = killedMonst + 1;
 				runBtn.disabled = true;
 				atackBtn.disabled = true;
 				keepGoingBtn.disabled = false;
-
 				console.log("\n" + killedMonst + " monsters are dead now");
 				console.log("You luck is " + yourLuck);
 			}
 				checkIfNotAWinner();
-				updateKilledCounter()
+				updateKilledCounter();
 	}
 								
 								
-function run()
-	{
+function run() {
 		var yourLuck;
 		yourLuck = Math.random();
 									
-		if (yourLuck < 0.5)
-			{
-											
-			storyLine.innerHTML = "You have been chased and now you have to fight!"
+		if (yourLuck < 0.5) {
+			storyLine.innerHTML = "You have been chased and now you have to fight!";
 			runBtn.disabled = true;
 			keepGoingBtn.disabled = true;	
 			}
 		
-		else if (yourLuck > 0.7)
-			{
-
-			storyLine.innerHTML = "You ran so fast that smashed 3 other monsters!"
+		else if (yourLuck > 0.7) {
+			storyLine.innerHTML = "You ran so fast that smashed 3 other monsters!";
 			runBtn.disabled = true;
 			atackBtn.disabled = true;
 			keepGoingBtn.disabled = false;
 			killedMonst = killedMonst + 3;
-
 			console.log("\n" + killedMonst + " monsters are dead now");
 			console.log("You luck is " + yourLuck);
 							
 			}
 		
-		else
-			{
-			storyLine.innerHTML = "You have run away!"
+		else {
+			storyLine.innerHTML = "You have run away!";
 			keepGoingBtn.disabled = false;
 			atackBtn.disabled = true;
 			runBtn.disabled = true;
 			}
 								
 			checkIfNotAWinner();
-			updateKilledCounter()
+			updateKilledCounter();
 	}
