b7c567d0cacf0ac850b56fa67fe6985882c1d602
unknown <vik.kammerer@gmail.com>
2017-11-27 15:21:08 +0200

    The code have been restructured and described

---
+++ UI_behavior.js
@@ -0,0 +1,81 @@
+//--------------------All UI elements' identifiers go here--------------------
+const yourHealthMonitor   =   document.getElementById("yourHealthMonitor");
+const storyLine           =   document.getElementById("storyLine");
+const runBtn              =   document.getElementById("run");
+const atackBtn            =   document.getElementById("attack");
+const keepGoingBtn        =   document.getElementById("keepGoing"); 
+const monstersLeftMonitor =   document.getElementById("monstersLeft");
+const yourLuckMonitor     =   document.getElementById("yourLuckMonitor");
+const tryAgain            =   document.getElementById("tryAgain");
+
+
+
+//--------------------Buttons' accessibility logic goes here------------------
+function setKeepGoing() {
+  runBtn.disabled = true;
+  atackBtn.disabled = true;
+  keepGoingBtn.disabled = false;
+}
+
+
+function setRunOrAttack() {
+  atackBtn.disabled = false;
+  runBtn.disabled = false;
+  keepGoingBtn.disabled = true;
+}
+
+function setAttackOnly() {
+  runBtn.disabled = true;
+  keepGoingBtn.disabled = true;
+}
+
+function setAllDisabled() {
+  runBtn.disabled = true;
+  atackBtn.disabled = true;
+  keepGoingBtn.disabled = true;
+}
+
+function displayTryAgainBtn() {
+  tryAgain.style.display = "block";
+} 
+
+
+
+//--------------------Counters' behavior goes here----------------------------
+function updateLuckMonitor() {
+  yourLuckMonitor.innerHTML = yourLuck;
+}
+
+function updateHealthMonitor() {
+  if (yourHealth >= 20 ) {
+  yourHealthMonitor.innerHTML = yourHealth;
+  }
+
+  else if (yourHealth < 20 && yourHealth > 0) {
+  yourHealthMonitor.innerHTML = yourHealth;
+  yourHealthMonitor.style.color = "red";
+  }
+
+  else{
+  yourHealthMonitor.innerHTML = 0;
+  }
+}
+
+function updateMonstersLeftMonitor() {
+  if (monstersLeft > 0)  {
+    monstersLeftMonitor.innerHTML = monstersLeft;
+    }
+    else
+    {
+      monstersLeft.innerHTML = "0";
+    }
+  }
+
+//--------------------StoryLine behavior goes here----------------------------
+function say(message) {
+  storyLine.innerHTML = message;
+}
+
+function refresh() {
+  say(enemyIsHereMessage);
+}
--- gameMechanic.js
+++ gameMechanic.js
@@ -1,8 +1,9 @@
+//--------------------Initial parameters go here-------------------------
 let monstersLeft			=		15;
 let yourHealth				=		100;
 let yourLuck;
 
-
+//--------------------Random luck generator goes here--------------------
 function getRandom() {
 	let rand = Math.floor(
      Math.random() * 100
@@ -18,10 +19,8 @@ function tryYourLuck() {
   return Math.floor(w/3);
 }
 
-function say(message) {
-  storyLine.innerHTML = message;
-}
 
+//-------------------------Game cases go here-----------------------------
 function haveBeenHurt() {
 	say(painMessage);
   yourHealth = yourHealth - 15;
@@ -50,39 +49,8 @@ function haveRunAway() {
   setKeepGoing();
 }
 
-function displayTryAgainBtn() {
-	tryAgain.style.display = "block";
-	}							
-								
-function updateMonstersLeftMonitor() {
-  if (monstersLeft > 0)  {
-	  monstersLeftMonitor.innerHTML = monstersLeft;
-    }
-    else
-    {
-      monstersLeft.innerHTML = "0";
-    }
-	}
 
-function updateHealthMonitor() {
-	if (yourHealth >= 20 ) {
-	yourHealthMonitor.innerHTML = yourHealth;
-	}
-
-	else if (yourHealth < 20 && yourHealth > 0) {
-	yourHealthMonitor.innerHTML = yourHealth;
-	yourHealthMonitor.style.color = "red";
-	}
-
-	else{
-	yourHealthMonitor.innerHTML = 0;
-	}
-}
-
-function updateLuckMonitor() {
-	yourLuckMonitor.innerHTML = yourLuck;
-}
-
+//-------------------------Final conclusions go here----------------------
 function checkIfNotAWinner() {
 		if (monstersLeft <= 0) {	
 			storyLine.append(winningMessage);
@@ -92,10 +60,6 @@ function checkIfNotAWinner() {
 	}
 
 								
-function refresh() {
-	say(enemyIsHereMessage);
-}
-
 function checkIfNotDead() {
 	if (yourHealth <= 0) {
 				say(deathMessage);
