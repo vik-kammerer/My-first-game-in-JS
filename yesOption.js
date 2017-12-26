82a58a54c43ed7d1814927890ec544cbff6c99fb
unknown <vik.kammerer@gmail.com>
2017-12-26 12:17:19 +0200

    Prior game case is now remembered

--- yesOption.js
+++ yesOption.js
@@ -3,47 +3,47 @@ function chooseYes() {
   updateLuckMonitor();
   if (gameCase === "chestDetected" && yourLuck >= 60) {
     elixirs++;
-    gameCase = undefined;
     updateElixirsMonitor();
     say(elixirFoundMessage);
     setRunOrAttackMode();
     setKeepGoing();
+    refreshStoryEvents();
   }
 
   else if (gameCase === "chestDetected" && yourLuck <= 40) {
     say(explodedChestMessage);
     yourHealth = yourHealth - 40;
-    gameCase = undefined;
     updateHealthMonitor();
     setRunOrAttackMode();
     setKeepGoing();
     checkIfNotDead();
+    refreshStoryEvents();
   }
 
   else if (gameCase === "chestDetected" && yourLuck > 40) {
-    gameCase = undefined;
     say(emptyChestMessage);
     setRunOrAttackMode();
     setKeepGoing();
+    refreshStoryEvents();
   }
 
   else if (gameCase === "prisonerDetected" && luckDecrement != 0) {
-    gameCase = undefined;
     say(beingBlessedMessage);
     luckDecrement = 0;
     updateLuckDecrementMonitor();
     updateLuckMonitor();
     setRunOrAttackMode();
     setKeepGoing();
+    refreshStoryEvents();
   }
 
   else if (gameCase === "prisonerDetected" && luckDecrement === 0) {
-    gameCase = undefined;
     say(beingBlessedMessage);
     yourHealth = yourHealth + 15;
     updateHealthMonitor();
     setRunOrAttackMode();
     setKeepGoing();
+    refreshStoryEvents();
   }
 
   else {
