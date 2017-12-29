48cc0d59d706fbcbbcb74ee6c11c091c64b4a042
unknown <vik.kammerer@gmail.com>
2017-12-29 14:42:14 +0200

    fat imp parameters has been added

--- enemies.js
+++ enemies.js
@@ -1,3 +1,14 @@
+
+//--------------------Stinky hoblin characteristics--------------------
 let   hoblinHealth    = 30;
 let   hoblinAttack    = 10;
 const hoblinName      = "a stinky hoblin";
+
+
+
+
+
+//--------------------Fat imp characteristics--------------------
+let   fatImpHealth    = 55;
+let   fatImpAttack    = 20;
+const fatImpName      = "a fat imp";
