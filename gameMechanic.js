36a66e1e9138dbfdb9219ae48e9537c36f16b018
unknown <vik.kammerer@gmail.com>
2018-04-05 15:56:06 +0300

    random luck generator has been improved

--- gameMechanic.js
+++ gameMechanic.js
@@ -9,19 +9,20 @@ let priorGameCase     =   undefined;
 let attackSkill       =   25;
 
 //--------------------Random luck generator goes here--------------------
-function getRandom() {
-	let rand = Math.floor(
-     Math.random() * 100
-   );
-	return rand;
+
+function getRandomIntInclusive(min, max) {
+
+    const randomBuffer = new Uint32Array(1);
+    window.crypto.getRandomValues(randomBuffer);
+    let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(randomNumber * (max - min + 1)) + min;
 }
 
 function tryYourLuck() {
-	let x = getRandom();
-  let z = getRandom();
-  let y = getRandom();
-  let w = x + z + y;
-  return Math.floor(w/3);
+  return getRandomIntInclusive(1, 100)
 }
 
 
